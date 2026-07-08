eacbce9d173b4645594fbb9ec0c98107cfd445d6
PrasannakumariLanka <honeyhoney2878@gmail.com>
2026-07-08 04:56:17 +0000

    Sync LeetCode submission Runtime - 23 ms (5.09%), Memory - 19.3 MB (37.33%)

--- 0728-self-dividing-numbers/solution.py
+++ 0728-self-dividing-numbers/solution.py
@@ -1,16 +1,17 @@
 class Solution:
-    def selfDividingNumbers(self, left: int, right: int) -> list[int]:
-        def is_self_dividing(n: int) -> bool:
-            original = n
-            while n > 0:
-                d = n % 10
-                if d == 0 or original % d != 0:
-                    return False
-                n //= 10
-            return True
-
-        result = []
-        for x in range(left, right + 1):
-            if is_self_dividing(x):
-                result.append(x)
-        return result
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        def snum(n:int)->bool:
+            count=0
+            for d in str(n):
+                if int(d)!=0:
+                    if n%(int(d))==0:
+                        count+=1
+            if count==len(str(n)):
+                return True
+            else:
+                return False
+        res=[] 
+        for i in range(left,right+1):
+            if snum(i)==True:
+                res.append(i)
+        return res
